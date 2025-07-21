bf3249653c087c41614f8f3a5281df402a4932f2
Han Gao <rabenda.cn@gmail.com>
2025-07-21 18:33:50 +0800

    REVYOS: HACK: xe: force DG1/DG2/ATS/PVC use xe driver
    Signed-off-by: Han Gao <rabenda.cn@gmail.com>

--- drivers/gpu/drm/xe/xe_pci.c
+++ drivers/gpu/drm/xe/xe_pci.c
@@ -269,7 +269,7 @@ static const struct xe_device_desc dg1_desc = {
 	.dma_mask_size = 39,
 	.has_display = true,
 	.has_heci_gscfi = 1,
-	.require_force_probe = true,
+	.require_force_probe = false,
 };
 
 static const u16 dg2_g10_ids[] = { INTEL_DG2_G10_IDS(NOP), INTEL_ATS_M150_IDS(NOP), 0 };
@@ -291,7 +291,7 @@ static const struct xe_device_desc ats_m_desc = {
 	.pre_gmdid_graphics_ip = &graphics_ip_xehpg,
 	.pre_gmdid_media_ip = &media_ip_xehpm,
 	.dma_mask_size = 46,
-	.require_force_probe = true,
+	.require_force_probe = false,
 
 	DG2_FEATURES,
 	.has_display = false,
@@ -301,7 +301,7 @@ static const struct xe_device_desc dg2_desc = {
 	.pre_gmdid_graphics_ip = &graphics_ip_xehpg,
 	.pre_gmdid_media_ip = &media_ip_xehpm,
 	.dma_mask_size = 46,
-	.require_force_probe = true,
+	.require_force_probe = false,
 
 	DG2_FEATURES,
 	.has_display = true,
@@ -317,7 +317,7 @@ static const __maybe_unused struct xe_device_desc pvc_desc = {
 	.has_display = false,
 	.has_heci_gscfi = 1,
 	.max_remote_tiles = 1,
-	.require_force_probe = true,
+	.require_force_probe = false,
 	.has_mbx_power_limits = false,
 };
 
